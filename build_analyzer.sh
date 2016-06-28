d4e7b1218fb92bbb54019ec3e77c958a9bb67635
Michael Bebenita <mbebenita@gmail.com>
2016-06-28 11:44:06 -0700

    Clean up analyzer build script.

--- build_analyzer.sh
+++ build_analyzer.sh
@@ -1,15 +1,14 @@
+echo Building Analyzer
 if [ ! -d "asm" ]; then
-  # Control will enter here if $DIRECTORY doesn't exist.
+  echo Configuring Analyzer
   mkdir asm
   cd asm && emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-experimental --enable-dering
 fi
 
 cd asm
 emmake make
-cp ../sc.ivf .
 cp examples/emscripten_decoder examples/emscripten_decoder.bc
-# emcc examples/emscripten_decoder.bc -o decoder.html --embed-file sc.ivf
 emcc -O3 examples/emscripten_decoder.bc -o examples/decoder.js -s TOTAL_MEMORY=134217728
-cd ..
-cp asm/examples/decoder.js ins/bin/decoder.js
-cp asm/examples/decoder.js.mem ins/bin/decoder.js.mem
+cp examples/decoder.js ../ins/bin/decoder.js
+cp examples/decoder.js.mem ../ins/bin/decoder.js.mem
+echo Analyzer is ready, serve it from the ins directory using your favorite web server. E.g. 'cd ins && python -m SimpleHTTPServer'

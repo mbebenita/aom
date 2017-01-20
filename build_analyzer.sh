52f542843a72b960cf9dc0ab2f76c753bcc49801
Michael Bebenita <mbebenita@mozilla.com>
2017-01-19 16:52:30 -0800

    Add build analyzer shell script.
    Change-Id: I6f9825d9765181fbbc049931c34ae134c689cbe2

---
+++ build_analyzer.sh
@@ -0,0 +1,12 @@
+echo Building Analyzer
+if [ ! -d "asm" ]; then
+  echo Configuring Analyzer
+  mkdir asm
+  cd asm && emconfigure ../configure --disable-multithread --disable-runtime-cpu-detect --target=generic-gnu --enable-accounting --enable-analyzer
+fi
+
+cd asm
+emmake make -j 8
+cp aomanalyzer aomanalyzer.bc
+emcc -O3 aomanalyzer.bc -o aomanalyzer.js -s TOTAL_MEMORY=134217728 -s MODULARIZE=1 -s EXPORT_NAME="'DecoderModule'" --post-js "../aomanalyzer-post.js" --memory-init-file 0
+cp aomanalyzer.js ../aomanalyzer.js
